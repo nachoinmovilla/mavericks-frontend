0abe83457cd00520888acb667f15c29a47e209a5
Ignacio Javier Tineo <itineo@idealista.com>
2024-05-03 10:33:24 +0200

    fix: key object

--- pages/propiedades.js
+++ pages/propiedades.js
@@ -12,7 +12,7 @@ const Propiedades = () => {
         <div className='h-screen w-full bg-blue-100'>
             <div className='bg-red-400'>
                 {PROPIEDADES_LISTA?.map((propiedad, index)=>(
-                    <div>
+                    <div key={index}>
                         {propiedad}
                     </div>
                 ))}
