61f9c3c06be0e35b7c4d37000a8b91aff4eb6daa
Ignacio Javier Tineo <itineo@idealista.com>
2024-05-03 10:31:05 +0200

    first commit

---
+++ pages/propiedades.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Propiedades = () => {
+
+    const PROPIEDADES_LISTA = [
+        'Propiedad 1',
+        'Propiedad 2', 
+        'Propiedad 3'
+    ]
+
+    return (
+        <div className='h-screen w-full bg-blue-100'>
+            <div className='bg-red-400'>
+                {PROPIEDADES_LISTA?.map((propiedad, index)=>(
+                    <div>
+                        {propiedad}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Propiedades;
